65c968ecbb269087ed9344f6190767a516830a75
olenamavdryk <olenamavdryk@gmail.com>
2025-02-01 01:12:26 +0100

    Add files via upload

---
+++ s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "step-project-3"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
